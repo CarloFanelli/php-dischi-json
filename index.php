930c74c2c83df47cafa83b8c3967ddaa2be56237
CarloFanelli <carlo.fanelli.sax@gmail.com>
2023-10-12 14:46:02 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+/* php code */
+?>
+
+<!doctype html>
+<html lang='en'>
+
+<head>
+    <meta charset='utf-8'>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <title>DISCHI</title>
+    <link href='./style.css' rel='stylesheet'>
+</head>
+
+<body>
+    <div id='app'>
+
+
+    </div>
+    <!-- Development only cdn, disable in production -->
+    <script src='https://unpkg.com/vue@3/dist/vue.global.js'></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/axios/1.5.1/axios.min.js" integrity="sha512-emSwuKiMyYedRwflbZB2ghzX8Cw8fmNVgZ6yQNNXXagFzFOaQmbvQ1vmDkddHjm5AITcBIZfC7k4ShQSjgPAmQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src='./main.js'></script>
+</body>
+
+</html>
